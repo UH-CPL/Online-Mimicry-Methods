f3aa53d44bf84cd6460f81532a379a9bce1c072e
Amanveer Wesley <amanveerwesley@gmail.com>
2022-05-27 05:50:25 -0500

    Shift from Treatment level to Participant Level

--- dc99_AddSummaryGaze_ParticipantLevel_to_AllStats_AllT_1HzMean_Clean.R
+++ dc99_AddSummaryGaze_ParticipantLevel_to_AllStats_AllT_1HzMean_Clean.R
@@ -30,42 +30,42 @@ for (sub in unique(d$Participant_ID)) {
   for (tr in unique(d1$Treatment)) {
     i = i+1
     cat(paste0(i, " ", tr,"\n"))
-
+    
     d2 = filter(d1, Treatment == tr)
-
+    
     df$Participant_ID[i] = sub
     df$Group[i] = unique(d2$Group)
     df$Treatment[i] = tr
-
+    
     na = colSums(is.na(d2[c(18:20)]))[2][[1]]/nrow(d2)*100
     nona = sum(table(d2$G_Direction))/nrow(d2)*100
     df$NAData[i] = na
     df$ValidData[i] = nona
     df$CenterPercentage1[i] = na + nona
-
+    
     x = prop.table(table(d2$G_Direction))*100
     df$CenterPercentage2[i] = sum(x)
     x = data.frame(t(data.frame(x)))
     colnames(x) = x[1,]
     x = x[-1,]
     x[1,] = as.numeric(x[1,])
-
+    
     if ("CLOSED" %in% colnames(x)) {
       df$Closed[i] = x$CLOSED
     }
-
+    
     if ("CENTER" %in% colnames(x)) {
       df$Center[i] = x$CENTER
     }
-
+    
     if ("LEFT" %in% colnames(x)) {
       df$Left[i] = x$LEFT
     }
-
+    
     if ("RIGHT" %in% colnames(x)) {
       df$Right[i] = x$RIGHT
     }
-
+    
   }
   
   cat("\n==========\n")
