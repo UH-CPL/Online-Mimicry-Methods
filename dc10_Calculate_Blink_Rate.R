3841055601c8ded7c0c4e3c4bbecc776eaee75b1
Amanveer Wesley <amanveerwesley@gmail.com>
2022-06-03 05:59:15 -0500

    Add Subject level visualization

--- dc10_Calculate_Blink_Rate.R
+++ dc10_Calculate_Blink_Rate.R
@@ -9,23 +9,15 @@ d = read.csv("Data/Presenter-Judges_FSPB+CJ+Gaze+ALLStats_AllT_1HzMean_Clean.csv
 d$Treatment[is.na(d$Treatment)] = "NA"
 d$Treatment_Time = ceiling(d$Treatment_Time)
 
-
 sink(paste0("Logs/CalculateBlinkRate.txt"))
 
-for (sub in unique(d$Participant_ID)) {
-  cat("\n")
-  cat(sub)
-  q = filter(d, Participant_ID == sub)
-  cat("\n")
-  cat(unique(q$Treatment))
-}
-
 for (sub in unique(d$Participant)) {
+    cat("\n\n\n\n==============================\n")
     cat(paste0("\n\n\n"),sub,"\n")
     d1 = filter(d, Participant_ID == sub)
     
     for (tr in unique(d1$Treatment)) {
-        cat(paste0(tr,"\n"))
+        cat(paste0("\n",tr,"\n"))
         d2 = filter(d1, Treatment == tr)
         if (tr == "NA") {
           d2$Treatment_Time = rep(seq(0, by = 1, length.out = nrow(d2)), each = 30)[1:nrow(d2)]
@@ -39,17 +31,13 @@ for (sub in unique(d$Participant)) {
         d2$BlinkStatus[which(d2$G_WOR >= 5.7)] = 1
         
         # plot(y = d2$BlinkStatus, x =  d2$Treatment_Time, type = "l", xlab = "Time [s]", ylab = "Blink Status", main = unique(d2$Treatment), ylim = c(0,1))
-        p = ggplot(d2, aes(x = Treatment_Time, y = BlinkStatus)) + geom_line() + theme_bw() + xlab("") + ylab(tr) + scale_y_continuous(breaks = c(0,1)) + theme(axis.title.y = element_text(angle = 0, face = "bold", size = 18))
+        p = ggplot(d2, aes(x = Treatment_Time, y = BlinkStatus)) + geom_line() + theme_bw() + xlab("") + ylab(tr) + scale_y_continuous(breaks = c(0,1)) + theme(axis.title.y = element_text(angle = 0, face = "bold", size = 13), plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))
         # ggplot(d3, aes(x = Treatment_Time, y = BlinkStatus)) + geom_line() + theme_bw() + xlab("") + ylab(tr) + scale_y_continuous(breaks = c(0,1)) + theme(axis.title.y = element_text(angle = 0, face = "bold", size = 18))
         
         if (tr == "PR") {
-          p = p + ylab("Time [s]")
+          p = p + xlab("Time [s]")
         }
-        
-        if (tr == "NA") {
-          p = p + ggtitle("Blink Status")
-        }
-        
+      
         assign(paste0("p",tr),p)
         
         mins = max(d2$Treatment_Time, na.rm = T)
@@ -68,14 +56,15 @@ for (sub in unique(d$Participant)) {
                   blrate = table(rle(d3$BlinkStatus)$values)[[2]]
                 }
                 if (dim(table(rle(d3$BlinkStatus)))[2] == 1) {
+                  if (is.na(unique(rle(d3$BlinkStatus)$values)[1])) {
+                    blrate = NA
+                    next
+                  }
                   if (unique(rle(d3$BlinkStatus)$values)[1] == 1) {
                     blrate = 999
                   }
                   if (unique(rle(d3$BlinkStatus)$values)[1] == 0) {
                     blrate = 0
-                  }
-                  if (is.na(unique(rle(d3$BlinkStatus)$values)[1])) {
-                    blrate = NA
                   }
                 }
                 d2$BlinkRate[which(d2$Treatment_Time >= start & d2$Treatment_Time <= end)] = blrate
@@ -103,12 +92,16 @@ for (sub in unique(d$Participant)) {
         }
     }
     pg = ggarrange(pNA, pRB, pST, pPM, pDT, pPR, nrow = 6)
+    pg = annotate_figure(pg, top = text_grob(paste0(sub,"  -  Blink Status"), face = "bold", size = 14))
+    
+
 }
 
-sink()
 
 ee = Sys.time()
 
 ee-ss
 
 write.csv(d, "Data/Presenter-Judges_FGSPB_AllT_1HzMean_SemiFinal_2022-04-06_withCompositeJBinary.csv", row.names = F)
+
+sink()
